a32d2198adcacc88abdb5257e45529a54d9ce889
Ivan Revyakin <LovingFox@GMail.com>
2022-10-22 20:38:24 +0300

    fix loop ip delete

--- down_tunnel.sh
+++ down_tunnel.sh
@@ -7,6 +7,8 @@ ID=$1 && shift
 ID5D=$(printf "%05d" $ID)
 WGIFACE=$(cat ${ID5D}_iface)
 TABLE=$(cat ${ID5D}_table)
+IP_LOOP=$(cat ${ID5D}_ip_loop)
 
 ip link delete $WGIFACE
 ip rule del pref $TABLE
+ip address delete $IP_LOOP/32 dev wgloop
