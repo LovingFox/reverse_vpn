431ab8332d5fd9b4ea93bbec3a449c3ca55e52d4
Ivan Revyakin <LovingFox@GMail.com>
2022-10-22 20:30:39 +0300

    looks good

--- show_tunnel.sh
+++ show_tunnel.sh
@@ -13,14 +13,14 @@ WGIFACE=$(cat ${ID5D}_iface)
 TABLE=$(cat ${ID5D}_table)
 IP_LOOP=$(cat ${ID5D}_ip_loop)
 
-echo "== Tunnel Interface"
+echo -e "\n== Tunnel Interface"
 ip -4 address show $WGIFACE
 
-echo "== Loop Interface"
+echo -e "\n== Loop Interface"
 ip -4 address show wgloop | grep -E "$IP_LOOP|wgloop:"
 
-echo "== Tunnel route"
+echo -e "\n== Tunnel route"
 ip -4 route show table $TABLE
 
-echo "== Wireguard"
+echo -e "\n== Wireguard"
 wg show $WGIFACE
