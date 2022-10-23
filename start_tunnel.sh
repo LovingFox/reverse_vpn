f9ad27117bd311dc6c46483532a4b2d4a453b328
Ivan Revyakin <LovingFox@GMail.com>
2022-10-23 17:19:35 +0300

    format actions

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -11,10 +11,10 @@ source "$BASE/source.sh"
 set_vars_files $ID
 vars_from_files
 
-echo -n "Creating $IFACE_LOCAL interface ($IP_LOCAL -> $IP_REMOTE, port $PORT_LOCAL)... "
+printf "%70s" "Creating $IFACE_LOCAL interface ($IP_LOCAL -> $IP_REMOTE, port $PORT_LOCAL)... "
 if ip link show dev $IFACE_LOCAL > /dev/null 2>&1
 then
-    echo "$IFACE_LOCAL exists. Skip"
+    echo "Skip, exists"
 else
     $SUDO ip link add dev $IFACE_LOCAL type wireguard
     $SUDO ip addr add $IP_LOCAL/31 dev $IFACE_LOCAL
@@ -24,19 +24,19 @@ else
     echo "Done"
 fi
 
-echo -n "Adding default -> $IFACE_LOCAL in $TAB_LOCAL table... "
+printf "%70s" "Adding default -> $IFACE_LOCAL in $TAB_LOCAL table... "
 if [[ $(ip route show table $TAB_LOCAL) ]]
 then
-    echo "$TAB_LOCAL not empty. Skip"
+    echo "Skip, not empty"
 else
     $SUDO ip route add default dev $IFACE_LOCAL table $TAB_LOCAL
     echo "Done"
 fi
 
-echo -n "Inserting ip rule from $IP_LOCAL with pref $PREF_LOCAL... "
+printf "%70s" "Inserting ip rule from $IP_LOCAL with pref $PREF_LOCAL... "
 if [[ $(ip rule show pref $PREF_LOCAL) ]]
 then
-    echo "$PREF_LOCAL exists. Skip"
+    echo "Skip, exists"
 else
     $SUDO ip rule add pref $PREF_LOCAL from $IP_LOCAL lookup $TAB_LOCAL
     echo "Done"
--- stop_tunnel.sh
+++ stop_tunnel.sh
@@ -11,19 +11,19 @@ source "$BASE/source.sh"
 set_vars_files $ID
 vars_from_files
 
-echo -n "Deleting $IFACE_LOCAL interface... "
+printf "%70s" "Deleting $IFACE_LOCAL interface... "
 if ! ip link show dev $IFACE_LOCAL > /dev/null 2>&1
 then
-    echo "$IFACE_LOCAL not found. Skip"
+    echo "Skip, not found"
 else
     $SUDO ip link delete $IFACE_LOCAL
     echo "Done"
 fi
 
-echo -n "Removing ip rule with pref $PREF_LOCAL... "
+printf "%70s" "Removing ip rule with pref $PREF_LOCAL... "
 if [[ ! $(ip rule show pref $PREF_LOCAL) ]]
 then
-    echo "$PREF_LOCAL not found. Skip"
+    echo "Skip, not found"
 else
     $SUDO ip rule del pref $TAB_LOCAL
     echo "Done"
